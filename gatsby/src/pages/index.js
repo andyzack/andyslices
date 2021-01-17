6533da64094502cc33fd54a146736948245986dc
Andrew Zacharia <andrew.zacharia@carterdigital.com>
2021-01-18 08:51:06 +1100

    Homepage fix

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -4,34 +4,15 @@ import Img from 'gatsby-image';
 import styled from 'styled-components';
 import SEO from '../components/SEO';
 
-const HomeGrid = styled.div`
-  transform: rotate(-2deg);
-  text-align: center;
-  font-size: 4rem;
-  margin-bottom: 4rem;
-  position: relative;
-  z-index: 2;
-`;
-
-const HomeStyles = styled.div`
-  background-color: var(--yellow);
-  padding: 1rem;
-  margin: 2rem;
-  margin-top: -6rem;
-  z-index: 2;
-  position: relative;
-  text-align: center;
-`;
-
 export default function HomePage() {
   return (
-    <HomeGrid>
+    <div>
       <SEO title="Home" />
-      <HomeStyles>
+      <div>
         <h1>Welcome to Andy's slices</h1>
         <p>Andy Serves the Sweetest Slices, incredibly the best in town :) </p>
         <Link to="/pizzas">Checkout the Menu and Order Now!!!</Link>
-      </HomeStyles>
-    </HomeGrid>
+      </div>
+    </div>
   );
 }
