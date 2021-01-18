533f2b269ae8e6d0d819b2b0ec036d4e8522817d
Andrew Zacharia <andrew.zacharia@carterdigital.com>
2021-01-18 12:24:46 +1100

    Homepage layout fix 2

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import { Link } from 'gatsby';
 import SEO from '../components/SEO';
 
 export default function HomePage() {
@@ -9,6 +9,7 @@ export default function HomePage() {
       <div>
         <h1>Welcome to Andy's slices</h1>
         <p>Andy Serves the Sweetest Slices, incredibly the best in town :) </p>
+        <Link to="/pizzas">Checkout the Menu and Order Now!!!</Link>
       </div>
     </>
   );
