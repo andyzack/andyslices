784e9741186e8c03d856f40e68e386f2cff12647
Andrew Zacharia <andrew.zacharia@carterdigital.com>
2021-01-18 12:15:55 +1100

    Homepage layout fix

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -2,27 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 import SEO from '../components/SEO';
 
-const HomePageGrid = styled.div`
-  background-color: var(--yellow);
-  padding: 2rem;
-  margin: 3rem;
-  margin-bottom: 6rem;
-  margin-top: -6rem;
-  z-index: 2;
-  position: relative;
-  transform: rotate(-2deg);
-  text-align: center;
-  font-size: 4rem;
-`;
-
 export default function HomePage() {
   return (
     <>
-      <HomePageGrid>
-        <SEO title="Home" />
+      <SEO title="Home" />
+      <div>
         <h1>Welcome to Andy's slices</h1>
         <p>Andy Serves the Sweetest Slices, incredibly the best in town :) </p>
-      </HomePageGrid>
+      </div>
     </>
   );
 }
