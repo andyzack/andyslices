ef3924ee14fb16ba1d2e7da77f52e60f7ae2ba95
Andrew Zacharia <andrew.zacharia@carterdigital.com>
2021-01-18 11:30:49 +1100

    Homepage restore

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import SEO from '../components/SEO';
 
 export default function HomePage() {
   return (
-    <div>
+    <>
       <SEO title="Home" />
-      <div>
-        <h1>Welcome to Andy's slices</h1>
-        <p>Andy Serves the Sweetest Slices, incredibly the best in town :) </p>
-        <Link to="/pizzas">Checkout the Menu and Order Now!!!</Link>
-      </div>
-    </div>
+      <h1>Welcome to Andy's slices</h1>
+      <p>Andy Serves the Sweetest Slices, incredibly the best in town :) </p>
+    </>
   );
 }
